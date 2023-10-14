d6de8719758e493ceb4e5cb58b9eb0972bce447a
GabrielDev <gabrys.wisniewski@op.pl>
2023-10-14 17:29:29 +0200

    version: demo + mobile / ready for publish

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
 import { KeyboardEvent, useState } from "react";
-import useLocalStorage from "use-local-storage";
+
 import Nav from "./components/nav";
-import { AiOutlineSearch} from "react-icons/ai";
+import { AiOutlineSearch } from "react-icons/ai";
 import { BiLinkExternal } from "react-icons/bi";
-import { FaPlay } from "react-icons/fa";
+import { BsFillVolumeUpFill } from "react-icons/bs";
 import "./styles/app.css";
-import './styles/nav.css'
+import "./styles/nav.css";
 import axios from "axios";
 
 function App() {
-  const [meanings, setMeanings] = useState("An associative array, a data structure where each value is referenced by a particular key, analogous to words and definitions in a physical dictionary.");
+  const [meanings, setMeanings] = useState(
+    "An associative array, a data structure where each value is referenced by a particular key, analogous to words and definitions in a physical dictionary."
+  );
   const [verbs, setVerbs] = useState("To complite a dictionary");
   const [titleResults, setTitleResults] = useState("Dictionary");
   const [readMainKey, setReadMainKey] = useState("/ˈdɪkʃəˌnɛɹi/");
-  const [sourceLink, setSourceLink] = useState("https://en.wiktionary.org/wiki/dictionary");
+  const [sourceLink, setSourceLink] = useState(
+    "https://en.wiktionary.org/wiki/dictionary"
+  );
   const [inputValue, setInputValue] = useState("");
   const [synoms, setSynoms] = useState("not found");
-  const [audio, setAudio] = useState("https://api.dictionaryapi.dev/media/pronunciations/en/dictionary-uk.mp3");
-  const [error, setError] = useState(false)
-  const [theme, setTheme] = useState("light")
-
+  const [audio, setAudio] = useState(
+    "https://api.dictionaryapi.dev/media/pronunciations/en/dictionary-uk.mp3"
+  );
+  const [error, setError] = useState(false);
+  const [theme, setTheme] = useState("light");
 
   const handlerDataApi = (e: KeyboardEvent, inputValue: string) => {
     if (e.code === "Enter") {
       setMeanings("");
       setVerbs("");
-      setError(false)
+      setError(false);
       axios
         .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
         .then((res) => {
@@ -36,8 +41,6 @@ function App() {
           setVerbs(res.data[0].meanings[1].definitions[2].definition);
           setSourceLink(res.data[0].sourceUrls[0]);
           setSynoms(res.data[0].meanings[0].synonyms[0]);
-
-        
 
           if (res.data[0].phonetics[0].audio === "") {
             setAudio(res.data[0].phonetics[1].audio);
@@ -50,32 +53,30 @@ function App() {
           } else {
             setAudio(res.data[0].phonetics[0].audio);
           }
-        }).catch((err) => {
-          if(err){
-            console.log("error working")
-            setError(!error)
-          } 
         })
+        .catch((err) => {
+          if (err) {
+            setError(!error);
+          }
+        });
     }
   };
 
   const playAudio = (audio: string) => {
     const play = new Audio(audio);
-    
+
     play.play();
-    
   };
 
-  const switchTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme)
-  }
-
+  const switchTheme = (theme: string) => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
 
   return (
     <div data-theme={theme} className="app">
-        <Nav switchTheme={switchTheme}/>
-      
+      <Nav switchTheme={() => switchTheme(theme)} />
+
       <main>
         <div className={error ? "active-error" : ""} id="search-box">
           <input
@@ -86,10 +87,12 @@ function App() {
             id="search"
             name="search"
             type="text"
-            placeholder="Enter the keyword"
+            placeholder="Enter the keyword and than click enter"
             onKeyDown={(e) => handlerDataApi(e, inputValue)}
           />
-          <span className={error ? "active-error-text" : "normal-text"}>{<AiOutlineSearch />}</span>
+          <span className={error ? "active-error-text" : "normal-text"}>
+            {<AiOutlineSearch />}
+          </span>
         </div>
         <div id="main-result-box">
           <section className="main-result-section" id="first">
@@ -101,7 +104,9 @@ function App() {
             </p>
           </section>
           <section className="main-result-section" id="btn-box">
-            <button onClick={() => playAudio(audio)}>{<FaPlay/>}</button>
+            <button onClick={() => playAudio(audio)}>
+              {<BsFillVolumeUpFill />}
+            </button>
           </section>
         </div>
         <div id="first-box-hr" className="box-hr">
@@ -139,20 +144,32 @@ function App() {
 
         <div id="source-box">
           <span>Source</span>
-          <a target="_blank" href={sourceLink}>{sourceLink}</a>
+          <a target="_blank" href={sourceLink}>
+            {sourceLink}
+          </a>
           <span>{<BiLinkExternal />}</span>
         </div>
         <div id="production-box">
           <section>
             <p className="simple-end-text">Coded by</p>
             <p className="by-text">
-              <a target="_blank" href="https://gabrli.github.io/GabrielDeveloperSite/">Gabriel Wiśniewski</a>
+              <a
+                target="_blank"
+                href="https://gabrli.github.io/GabrielDeveloperSite/"
+              >
+                Gabriel Wiśniewski
+              </a>
             </p>
           </section>
           <section>
             <p className="simple-end-text">The challenge comes from</p>
             <p className="by-text">
-              <a target="_blank" href="https://www.frontendmentor.io/challenges/dictionary-web-app-h5wwnyuKFL">Frontend Mentor | Challenges</a>
+              <a
+                target="_blank"
+                href="https://www.frontendmentor.io/challenges/dictionary-web-app-h5wwnyuKFL"
+              >
+                Frontend Mentor | Challenges
+              </a>
             </p>
           </section>
         </div>
--- src/types/type.tsx
+++
@@ -1,5 +0,0 @@
-interface meanings {
-    definition: string
-}
-
-export type {meanings}
